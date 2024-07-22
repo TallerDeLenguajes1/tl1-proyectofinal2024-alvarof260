f423ebb264f33fc8a0d60201551aa599613c0e31
alvarof260 <alvarof260@gmail.com>
2024-07-22 00:27:32 -0300

    feat: menu inicial, basico

--- Program.cs
+++ Program.cs
@@ -4,6 +4,41 @@ public class Program
 {
   public static void Main()
   {
-    Console.WriteLine("Hola Mundo!");
+    Inicio();
+  }
+
+  public static void Inicio()
+  {
+    int opcion;
+    string input;
+    bool seleccionado;
+    do
+    {
+      //! TODO: TRATAR DE HACER CONTROL DE PARTIDAS SIN TERMINAR  
+      Console.WriteLine("Bienvenidos a Conquista y Triunfa");
+      Console.WriteLine("1 - Nueva Partida");
+      Console.WriteLine("2 - Historial");
+      Console.WriteLine("3 - Salir");
+      input = Console.ReadLine();
+      seleccionado = int.TryParse(input, out opcion);
+      if (opcion < 1 || opcion > 3 || !seleccionado)
+      {
+        Console.WriteLine("Opcion no Valida!");
+      }
+    } while (!seleccionado || (opcion < 1 || opcion > 3));
+
+    switch (opcion)
+    {
+      case 1:
+        //! FALTA HACER EL JUEGO
+        break;
+      case 2:
+        //! FALTA HACER EL CONTROL DE PARTIDAS
+        break;
+      case 3:
+        Console.WriteLine("Saliendo del juego...");
+        Environment.Exit(0);
+        break;
+    }
   }
 }
