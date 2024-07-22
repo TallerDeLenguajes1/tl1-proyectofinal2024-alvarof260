831718c3e0092182ae60772064a0f4db2648c646
alvarof260 <alvarof260@gmail.com>
2024-07-22 02:14:05 -0300

    feat: mejoras a nivel de codigo

--- src/juego/Jugador.cs
+++ src/juego/Jugador.cs
@@ -1,17 +1,50 @@
+using System;
+
 namespace Proyecto
 {
     public class Jugador
     {
-        public string Tipo { set; get; }
-        public string Nombre { set; get; }
-        public string Apodo { set; get; }
-        public DateTime FechaDeNacimiento { set; get; }
-        public int Edad { set; get; }
-        public int Velocidad { set; get; }
-        public int Destreza { set; get; }
-        public int Fuerza { set; get; }
-        public int Nivel { set; get; }
-        public int Armadura { set; get; }
-        public int Salud = 100;
+        public string Tipo { get; set; }
+        public string Nombre { get; set; }
+        public string Apodo { get; set; }
+        public DateTime FechaDeNacimiento { get; set; }
+        public int Edad
+        {
+            get
+            {
+                DateTime hoy = DateTime.Today;
+                int edad = hoy.Year - FechaDeNacimiento.Year;
+                if (FechaDeNacimiento.Date > hoy.AddYears(-edad)) edad--;
+                return edad;
+            }
+        }
+
+        public int Velocidad { get; set; }
+        public int Destreza { get; set; }
+        public int Fuerza { get; set; }
+        public int Nivel { get; set; }
+        public int Armadura { get; set; }
+        public int Salud { get; private set; } = 100;
+
+        private static readonly Random random = new Random();
+
+        public Jugador(string tipo, string nombre, string apodo, DateTime fechaDeNacimiento)
+        {
+            Tipo = tipo;
+            Nombre = nombre;
+            Apodo = apodo;
+            FechaDeNacimiento = fechaDeNacimiento;
+
+            Velocidad = GenerarStats(1, 10);
+            Destreza = GenerarStats(1, 10);
+            Fuerza = GenerarStats(1, 10);
+            Nivel = GenerarStats(1, 10);
+            Armadura = GenerarStats(1, 10);
+        }
+
+        public static int GenerarStats(int min, int max)
+        {
+            return random.Next(min, max + 1);
+        }
     }
 }
