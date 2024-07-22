c7cfeb391b04941875a4a3af1508870704a0958f
alvarof260 <alvarof260@gmail.com>
2024-07-21 23:48:53 -0300

    resetear proyecto

--- Program.cs
+++ Program.cs
@@ -1,35 +1,9 @@
 ﻿using System;
 
-namespace EldenRing
+public class Program
 {
-  class Program
+  public static void Main()
   {
-    static void Main()
-    {
-      Task.Run(Start).GetAwaiter().GetResult();
-    }
-
-    static async Task Start()
-    {
-      Console.WriteLine("Bienvenido a Elden Ring RPG Console");
-      // URL de la API de Elden Ring
-      string apiUrl = "https://eldenring.fanapis.com/api/classes";
-
-      // Crear una instancia de ApiClient
-      var apiClient = new ApiClient(apiUrl);
-
-      // Obtener datos de la API
-      List<Datos> personajes = await apiClient.GetPersonajesAsync();
-      string archivoJson = "personajes.json";
-      JsonDB.GuardanEnJson(personajes, archivoJson);
-
-      List<Datos> personajesGuardado = JsonDB.LeerEnJson(archivoJson);
-
-      foreach (var personaje in personajesGuardado)
-      {
-        Console.WriteLine(personaje.Nombre);
-      }
-
-    }
+    Console.WriteLine("Hola Mundo!");
   }
 }
